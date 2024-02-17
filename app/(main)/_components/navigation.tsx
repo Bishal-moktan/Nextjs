888fe08087cfde5c9e7e2e167b0503345d9e380c
Bishal Moktan <busial.mok9@gmail.com>
2024-02-17 22:12:34 +0530

    Added convex for backend and clerk for github authentication

---
+++ app/(main)/_components/navigation.tsx
@@ -0,0 +1,10 @@
+const Navigation = () => {
+  return (
+    <>
+      <aside className="group/sidebar h-full bg-secondary overflow-y-scroll relative flex w-60 flex-col z-[99999]">
+        Navigation
+      </aside>
+    </>
+  );
+};
+export default Navigation;
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Spinner from '@/components/spinner';
+import { useConvexAuth } from 'convex/react';
+import { redirect } from 'next/navigation';
+import React from 'react';
+import Navigation from './_components/navigation';
+
+const MainPageLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isLoading, isAuthenticated } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex justify-center items-center">
+        <Spinner size={'lg'} />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect('/');
+  }
+  return (
+    <div className="h-full flex dark:bg-[#1f1f1f]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-scroll">{children}</main>
+    </div>
+  );
+};
+export default MainPageLayout;
